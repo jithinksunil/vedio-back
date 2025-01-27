e840d43e235b6c80d1e851beba5c8ac00d2ef04d
jithinksunil <jithinksunil96@gmail.com>
2025-01-27 16:02:23 +0530

    updates

--- dist/helper/helper.service.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-export declare class HelperService {
-    generateFileName(uniqueKey: string): string;
-    combineImagesVertically(bufferArray: Buffer[], uniqueKey: string): Promise<Buffer>;
-}
--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,34 +1,36 @@
 import {
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-  } from '@nestjs/websockets';
-  import { Server, Socket } from 'socket.io';
-  
-  @WebSocketGateway({ cors: { origin: '*' } }) // Enable CORS for all origins
-  export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer()
-    server: Server;
-  
-    // When a client connects
-    handleConnection(client: Socket) {
-      console.log(`Client connected: ${client.id}`);
-    }
-  
-    // When a client disconnects
-    handleDisconnect(client: Socket) {
-      console.log(`Client disconnected: ${client.id}`);
-    }
-  
-    // Listen for 'control' events from clients
-    @SubscribeMessage('control')
-    handleControl(client: Socket, payload: { type: string; time?: number }) {
-      console.log(`Received control command:`, payload);
-  
-      // Broadcast the control event to all connected clients
-      this.server.emit('control', payload);
-    }
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: { origin: '*' } }) // Enable CORS for all origins
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  // When a client connects
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
   }
-  
+
+  // When a client disconnects
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  // Listen for 'control' events from clients
+  @SubscribeMessage('control')
+  handleControl(
+    client: Socket,
+    payload: { type: string; time?: number; seekSetter: number }
+  ) {
+    console.log(`Received control command:`, payload);
+
+    // Broadcast the control event to all connected clients
+    this.server.emit('control', payload);
+  }
+}
